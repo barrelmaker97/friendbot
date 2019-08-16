0eeb39fb19c179c4c3025265294b553ce7843f5e
Nolan Cooper <nolan.cooper97@motorolasolutions.com>
2019-08-16 09:36:21 -0500

    ✨ Accept POST requests

--- app.py
+++ app.py
@@ -12,8 +12,14 @@ def user():
         corpus = friend.generateCorpus("../export", channel, userID)
         sentences = friend.generateSentence(corpus, 1)
         return sentences[0]
-    if request.method == 'GET':
+    if request.method == 'POST':
         data = request.form # a multidict containing POST data
+        params = data['text'].split()
+        channel = friend.getChannel(params[0])
+        userID = friend.getUserID(params[1])
+        corpus = friend.generateCorpus("../export", channel, userID)
+        sentences = friend.generateSentence(corpus, 1)
+        return sentences[0]
 
 if __name__ == '__main__':
     app.run()
