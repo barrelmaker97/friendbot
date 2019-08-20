6bf345c7a8be5686bd76e7cfb691eb31c1cc9e98
Nolan Cooper <nolancooper97@gmail.com>
2019-08-20 18:48:30 -0500

    ✅ Use JSON and return 200 for certain errors

--- features/sentence.feature
+++ features/sentence.feature
@@ -8,12 +8,12 @@ Feature: Generating Sentences
 	Scenario: Request a sentence for a missing channel
 		Given friendbot is running
 		When we make a POST request for channel TEST user all at the /sentence endpoint
-		Then we will get a 400 status code
+		Then we will get a 200 status code
 
 	Scenario: Request a sentence for a missing user
 		Given friendbot is running
 		When we make a POST request for channel all user TEST at the /sentence endpoint
-		Then we will get a 400 status code
+		Then we will get a 200 status code
 
 	Scenario: Request a sentence with the wrong method
 		Given friendbot is running
--- friendbot/routes.py
+++ friendbot/routes.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, jsonify
 from friendbot import app, corpus
 
 export = app.config['EXPORT_DIR']
@@ -9,9 +9,12 @@ def create_sentence():
     params = data['text'].split()
     try:
         channel = corpus.getChannel(params[0], export)
+    except:
+        return jsonify(text="Channel not found")
+    try:
         userID = corpus.getUserID(params[1])
     except:
-        return "Bad request", 400
+        return jsonify(text="User not found")
     fulltext = corpus.generateCorpus(export, channel, userID)
     sentence = corpus.generateSentence(fulltext)
-    return sentence
+    return jsonify(text=sentence)
