521ab8826b201b4967e37980ca80c45590aa0bff
Nolan Cooper <nolancooper97@gmail.com>
2019-09-27 23:23:40 -0500

    Update docker command

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 if [[ $IMAGE_TAG == "arm32v7" ]]; then
 	docker run --rm --privileged multiarch/qemu-user-static:register
+	docker run -it --rm multiarch/debian-debootstrap:armhf-jessie
 fi
