49b4e9a04ade1ccf092c011c247fb410322807b5
Nolan Cooper <nolancooper97@gmail.com>
2019-09-27 22:56:11 -0500

    Pull apart script

--- docker-setup.sh
+++ docker-setup.sh
@@ -2,5 +2,3 @@
 if [[ $IMAGE_TAG == "arm32v7" ]]; then
 	docker run --rm --privileged multiarch/qemu-user-static:register
 fi
-docker build -t barrelmaker97/friendbot:$IMAGE_TAG --build-arg BASE_IMAGE=$BASE_IMAGE .
-docker run -d -p 127.0.0.1:5000:5000 -v $(realpath ./test_data/export):/export barrelmaker97/friendbot:$IMAGE_TAG
