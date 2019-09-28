7b329330c4624f6f73580d951c85e36451f1e075
Nolan Cooper <nolancooper97@gmail.com>
2019-09-27 22:45:59 -0500

    Build ARM version of image

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 coveralls
 echo "$DOCKER_PASSWORD" | docker login -u "barrelmaker97" --password-stdin
-docker push barrelmaker97/friendbot:latest
+docker push barrelmaker97/friendbot:$IMAGE_TAG
