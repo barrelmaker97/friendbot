acbe3ade0c53171076c01ece08ff14701ea20f51
Nolan Cooper <nolancooper97@gmail.com>
2019-10-01 22:33:16 -0500

    Include user_id in tests

--- features/steps/sentence_steps.py
+++ features/steps/sentence_steps.py
@@ -3,21 +3,21 @@ from behave import when, then
 
 @when("we make a POST request for {arg0} and {arg1} at {endpoint}")
 def post_two_args(context, arg0, arg1, endpoint):
-    data = "{} {}".format(arg0, arg1)
-    context.res = context.client.post(endpoint, data=dict(text=data))
+    text = "{} {}".format(arg0, arg1)
+    context.res = context.client.post(endpoint, data=dict(text=text, user_id="UCF55PTPV"))
     assert context.res
 
 
 @when("we make a POST request for {arg0} at {endpoint}")
 def post_one_arg(context, arg0, endpoint):
-    data = arg0
-    context.res = context.client.post(endpoint, data=dict(text=data))
+    text = arg0
+    context.res = context.client.post(endpoint, data=dict(text=text, user_id="UCF55PTPV"))
     assert context.res
 
 
 @when("we make a blank POST request at {endpoint}")
 def post_endpoint_blank(context, endpoint):
-    context.res = context.client.post(endpoint, data=dict(text=""))
+    context.res = context.client.post(endpoint, data=dict(text="", user_id="UCF55PTPV"))
     assert context.res
 
 
