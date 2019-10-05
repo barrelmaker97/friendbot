d56cf44d967c5e8feb6911be756f1a2b62bb7980
Nolan Cooper <nolancooper97@gmail.com>
2019-10-04 23:39:36 -0500

    Pull apart scripts

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "barrelmaker97" --password-stdin
-docker push barrelmaker97/friendbot:$IMAGE_TAG
--- docker-setup.sh
+++ docker-setup.sh
@@ -4,5 +4,3 @@ if [[ $IMAGE_TAG == "arm" ]]; then
 	wget https://github.com/multiarch/qemu-user-static/releases/download/v4.1.0-1/qemu-arm-static.tar.gz -O ./qemu-arm-static.tar.gz
 	tar zxvf ./qemu-arm-static.tar.gz -C ./
 fi
-docker build -t barrelmaker97/friendbot:$IMAGE_TAG --build-arg BASE_IMAGE=$BASE_IMAGE .
-docker run -d -p 127.0.0.1:5000:5000 -v $(realpath ./test_data/export):/export barrelmaker97/friendbot:$IMAGE_TAG
