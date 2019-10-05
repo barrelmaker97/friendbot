cc583c21521ed56fb36a02f2bda9b72b037f30ce
Nolan Cooper <nolancooper97@gmail.com>
2019-10-04 23:05:12 -0500

    Fix docker setup for arm

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ $IMAGE_TAG == "arm32v7" ]]; then
+if [[ $IMAGE_TAG == "arm" ]]; then
 	docker run --rm --privileged multiarch/qemu-user-static:register
 	wget https://github.com/multiarch/qemu-user-static/releases/download/v4.1.0-1/qemu-arm-static.tar.gz -O ./qemu-arm-static.tar.gz
 	tar zxvf ./qemu-arm-static.tar.gz -C ./
