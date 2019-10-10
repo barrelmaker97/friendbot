37bc3ea2fe86f3d410fa0a36d3b486312c8bcd10
Nolan Cooper <nolancooper97@gmail.com>
2019-10-09 21:40:49 -0500

    Attempt to parallelize build

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "barrelmaker97" --password-stdin
-docker push barrelmaker97/friendbot:$IMAGE_TAG
+docker push barrelmaker97/friendbot:amd64
+docker push barrelmaker97/friendbot:arm
