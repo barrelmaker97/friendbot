5636b785dd87205a31c5636b790b6276870aeb8b
Nolan Cooper <nolancooper97@gmail.com>
2019-10-15 18:19:37 -0500

    Quotes to prevent globbing/splitting

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export DOCKER_CLI_EXPERIMENTAL=enabled
 echo "$DOCKER_PASSWORD" | docker login -u "barrelmaker97" --password-stdin
-docker push barrelmaker97/friendbot:$IMAGE_TAG
+docker push barrelmaker97/friendbot:"$IMAGE_TAG"
 docker manifest create barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 barrelmaker97/friendbot:arm
 docker manifest annotate barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 --arch amd64
 docker manifest annotate barrelmaker97/friendbot:latest barrelmaker97/friendbot:arm --arch arm
