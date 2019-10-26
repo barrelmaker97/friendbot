16e6f191042f06c0fc55fc1c513892d3280a2bf6
Nolan Cooper <nolancooper97@gmail.com>
2019-10-26 11:24:46 -0500

    Reformat __init__

--- friendbot/__init__.py
+++ friendbot/__init__.py
@@ -1,6 +1,5 @@
 import logging
 from flask import Flask
-from os import environ, path
 from friendbot import corpus
 
 app = Flask(__name__)
@@ -10,24 +9,38 @@ if __name__ != "__main__":
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
-app.config["EXPORT"] = "/export_unzip"
+export = "/export_unzip"
 
 try:
-    app.config["CHANNEL_DICT"] = corpus.getChannelDict(app.config["EXPORT"])
-    app.config["CHANNELS"] = app.config["CHANNEL_DICT"].keys()
+    user_dict = corpus.getUserDict(export)
+    users = user_dict.keys()
+    app.logger.info("Users loaded from export")
+except Exception as ex:
+    msg = "An exception of type {} occurred. Users not loaded!"
+    format_msg = msg.format(type(ex).__name__)
+    app.logger.error(format_msg)
+
+try:
+    channel_dict = corpus.getChannelDict(export)
+    channels = channel_dict.keys()
     app.logger.info("Channels loaded from export")
 except Exception as ex:
     msg = "An exception of type {} occurred. Channels not loaded!"
     format_msg = msg.format(type(ex).__name__)
     app.logger.error(format_msg)
 
-try:
-    app.config["USER_DICT"] = corpus.getUserDict(app.config["EXPORT"])
-    app.config["USERS"] = app.config["USER_DICT"].keys()
-    app.logger.info("Users loaded from export")
-except Exception as ex:
-    msg = "An exception of type {} occurred. Users not loaded!"
-    format_msg = msg.format(type(ex).__name__)
-    app.logger.error(format_msg)
+# count = 1
+# for user in users:
+#    for channel in channels:
+#        app.logger.info("Generating model for {} {}".format(user, channel))
+#        corpus._generateCorpus(export, user, channel, user_dict, channel_dict)
+#        count += 1
+# app.logger.info("Generated {} models".format(count))
+
+app.config["EXPORT"] = export
+app.config["USER_DICT"] = user_dict
+app.config["USERS"] = users
+app.config["CHANNEL_DICT"] = channel_dict
+app.config["CHANNELS"] = channels
 
 from friendbot import routes
