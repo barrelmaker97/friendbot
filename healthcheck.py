efcfa0cf4d9bdfaa030fac89c10384ccc60e4d16
Nolan Cooper <nolancooper97@gmail.com>
2020-04-15 18:43:55 -0500

    Add healthcheck

---
+++ healthcheck.py
@@ -0,0 +1,7 @@
+import requests as r
+
+payload = { "text": "", "user_id": "UCF55PTPV"}
+endpoint = "http://localhost:5000/sentence"
+resp = r.post(endpoint, payload)
+resp.raise_for_status()
+assert resp.headers["Friendbot-Error"] == "False"
