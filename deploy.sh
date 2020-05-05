540db6d289271a6349bffa84be9a68987df1a790
Nolan Cooper <nolancooper97@gmail.com>
2020-05-05 02:14:35 +0000

    Remove IMAGE_TAG from deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKER_CLI_EXPERIMENTAL=enabled
 echo "$DOCKER_PASSWORD" | docker login -u "barrelmaker97" --password-stdin
-docker push barrelmaker97/friendbot:"$IMAGE_TAG"
+docker push barrelmaker97/friendbot
 docker manifest create barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 barrelmaker97/friendbot:arm64v8
 docker manifest push barrelmaker97/friendbot:latest
