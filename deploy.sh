7810292e70fa7bf92b9c77a2b0df797fd6f43ce3
Nolan Cooper <nolancooper97@gmail.com>
2020-05-05 02:26:25 +0000

    Build arm32 and arm64 images

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 export DOCKER_CLI_EXPERIMENTAL=enabled
 echo "$DOCKER_PASSWORD" | docker login -u "barrelmaker97" --password-stdin
 docker push barrelmaker97/friendbot
-docker manifest create barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 barrelmaker97/friendbot:arm64v8
+docker manifest create barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 barrelmaker97/friendbot:arm64v8 barrelmaker97/friendbot:arm32v7
 docker manifest push barrelmaker97/friendbot:latest
