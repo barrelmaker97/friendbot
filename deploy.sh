106001f166cc3ef671fbfb432a8bf1f4fe89b35e
Nolan Cooper <nolancooper97@gmail.com>
2020-05-05 00:19:53 +0000

    Remove Docker manifest annotate

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,4 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 echo "$DOCKER_PASSWORD" | docker login -u "barrelmaker97" --password-stdin
 docker push barrelmaker97/friendbot:"$IMAGE_TAG"
 docker manifest create barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 barrelmaker97/friendbot:arm64v8
-docker manifest annotate barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 --arch amd64
-docker manifest annotate barrelmaker97/friendbot:latest barrelmaker97/friendbot:arm64v8 --arch arm64v8
 docker manifest push barrelmaker97/friendbot:latest
