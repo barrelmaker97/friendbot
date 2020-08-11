338a5e9c2cdbe0ce79fc2ee92a2002ac1243b120
Nolan Cooper <nolancooper97@gmail.com>
2020-08-10 20:45:24 -0500

    Generate urlencoded request body in healthcheck

--- healthcheck.py
+++ healthcheck.py
@@ -3,12 +3,13 @@ import time
 import hmac
 import hashlib
 import os
+import urllib
 
 endpoint = "http://localhost:6000/sentence"
 payload = {"text": "", "user_id": "healthcheck"}
 signing_secret = os.environ.get("SLACK_SIGNING_SECRET")
 if signing_secret is not None:
-    request_body = "text=&user_id=healthcheck"
+    request_body = urllib.parse.urlencode(payload)
     timestamp = str(int(time.time()))
     slack_basestring = f"v0:{timestamp}:{request_body}".encode("utf-8")
     slack_signing_secret = bytes(signing_secret, "utf-8")
