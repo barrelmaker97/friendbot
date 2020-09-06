4a77569bc5838a0fef34dfd56f3e7047ed0ba17b
Nolan Cooper <nolancooper97@gmail.com>
2020-09-06 15:00:38 -0500

    Convert messages to string

--- friendbot/messages.py
+++ friendbot/messages.py
@@ -1,61 +1,78 @@
+import ujson
+
+
 def errorMessage():
-    return {
-        "response_type": "ephemeral",
-        "replace_original": False,
-        "text": "Sorry, that didn't work. Please try again.",
-    }
+    return ujson.dumps(
+        {
+            "response_type": "ephemeral",
+            "replace_original": False,
+            "text": "Sorry, that didn't work. Please try again.",
+        }
+    )
 
 
 def cancelMessage():
-    return {"delete_original": True}
+    return ujson.dumps({"delete_original": True})
 
 
 def sendMessage(sentence, real_name):
     context_msg = f"Sent by {real_name}"
-    return {
-        "delete_original": True,
-        "response_type": "in_channel",
-        "blocks": [
-            {"type": "section", "text": {"type": "plain_text", "text": sentence}},
-            {
-                "type": "context",
-                "elements": [{"type": "plain_text", "text": context_msg}],
-            },
-        ],
-    }
+    return ujson.dumps(
+        {
+            "delete_original": True,
+            "response_type": "in_channel",
+            "blocks": [
+                {"type": "section", "text": {"type": "plain_text", "text": sentence}},
+                {
+                    "type": "context",
+                    "elements": [{"type": "plain_text", "text": context_msg}],
+                },
+            ],
+        }
+    )
 
 
 def promptMessage(sentence, user, channel):
-    return {
-        "replace_original": True,
-        "response_type": "ephemeral",
-        "blocks": [
-            {"type": "section", "text": {"type": "plain_text", "text": sentence}},
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {"type": "plain_text", "emoji": True, "text": "Send"},
-                        "style": "primary",
-                        "value": sentence,
-                    },
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": True,
-                            "text": "Shuffle",
+    return ujson.dumps(
+        {
+            "replace_original": True,
+            "response_type": "ephemeral",
+            "blocks": [
+                {"type": "section", "text": {"type": "plain_text", "text": sentence}},
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "emoji": True,
+                                "text": "Send",
+                            },
+                            "style": "primary",
+                            "value": sentence,
                         },
-                        "value": f"{user} {channel}",
-                    },
-                    {
-                        "type": "button",
-                        "text": {"type": "plain_text", "emoji": True, "text": "Cancel"},
-                        "style": "danger",
-                        "value": "cancel",
-                    },
-                ],
-            },
-        ],
-    }
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "emoji": True,
+                                "text": "Shuffle",
+                            },
+                            "value": f"{user} {channel}",
+                        },
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "emoji": True,
+                                "text": "Cancel",
+                            },
+                            "style": "danger",
+                            "value": "cancel",
+                        },
+                    ],
+                },
+            ],
+        }
+    )
