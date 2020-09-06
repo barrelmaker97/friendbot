29719da27b3ca4e7582082ef594d091966ffa59c
Nolan Cooper <nolancooper97@gmail.com>
2020-09-06 19:14:05 +0000

    Remove Arm32v7 build

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,7 @@
 export DOCKER_CLI_EXPERIMENTAL=enabled
 echo "$DOCKER_PASSWORD" | docker login -u "barrelmaker97" --password-stdin
 docker push barrelmaker97/friendbot
-docker manifest create barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 barrelmaker97/friendbot:arm64v8 barrelmaker97/friendbot:arm32v7
+docker manifest create barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 barrelmaker97/friendbot:arm64v8
 docker manifest annotate barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 --os linux --arch amd64
 docker manifest annotate barrelmaker97/friendbot:latest barrelmaker97/friendbot:arm64v8 --os linux --arch arm64 --variant v8
-docker manifest annotate barrelmaker97/friendbot:latest barrelmaker97/friendbot:arm32v7 --os linux --arch arm --variant v7
 docker manifest push barrelmaker97/friendbot:latest
