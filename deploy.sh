d7a96724d295a26e1711568bba9da16deb6c891e
Nolan Cooper <nolancooper97@gmail.com>
2020-09-20 13:55:51 -0500

    Clean up old files from Travis

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DOCKER_CLI_EXPERIMENTAL=enabled
-echo "$DOCKER_PASSWORD" | docker login -u "barrelmaker97" --password-stdin
-docker push barrelmaker97/friendbot
-docker manifest create barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 barrelmaker97/friendbot:arm64v8
-docker manifest annotate barrelmaker97/friendbot:latest barrelmaker97/friendbot:amd64 --os linux --arch amd64
-docker manifest annotate barrelmaker97/friendbot:latest barrelmaker97/friendbot:arm64v8 --os linux --arch arm64 --variant v8
-docker manifest push barrelmaker97/friendbot:latest
--- setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker network create build_net
-docker run -d --name redis --network build_net redis:alpine
