ea64c6e9d7ad2bc289cf78375ad54ac1bc7c26e1
Nolan Cooper <nolancooper97@gmail.com>
2020-09-28 14:48:24 -0500

    Revert "Try testing without Redis as well"
    This reverts commit 3da677f7ec25a7c8ebaff1711b55aef8149111b3.

--- features/environment.py
+++ features/environment.py
@@ -1,6 +1,5 @@
 from behave import fixture, use_fixture
 from friendbot import app
-import os
 
 
 @fixture
@@ -12,7 +11,4 @@ def flaskr_client(context, *args, **kwargs):
 
 def before_tag(context, tag):
     if tag == "PostStart":
-        use_fixture(flaskr_client, context)
-    if tag == "PostStartNoRedis":
-        os.environ["FRIENDBOT_REDIS_HOST"] = "notredis"
         use_fixture(flaskr_client, context)
