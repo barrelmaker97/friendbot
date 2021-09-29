7ecd4f755dc74a7adc2c5a6514e88ae7550bc0c6
Nolan Cooper <nolancooper97@gmail.com>
2021-09-28 23:16:34 -0500

    Remove another install

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/ash
 pip install --upgrade pip --no-cache-dir
 pip install -r requirements.txt --no-cache-dir
-apk add --no-cache libstdc++
 adduser --disabled-password --gecos "" --uid "1234" "friendbot"
 rm /install.sh /requirements.txt
