73982a62ab4fedf40c70b16303cc839d3b467a23
Nolan Cooper <nolancooper97@gmail.com>
2021-10-11 18:42:35 -0500

    Read config at startup

--- friendbot/__init__.py
+++ friendbot/__init__.py
@@ -9,6 +9,12 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 app = Flask(__name__)
 
+# Read configuration from environment
+signing_secret_file = os.environ.get("FRIENDBOT_SECRET_FILE")
+export_zip = os.environ.get("FRIENDBOT_EXPORT_ZIP", "/home/friendbot/export.zip")
+redis_host = os.environ.get("FRIENDBOT_REDIS_HOST", "redis")
+redis_port = os.environ.get("FRIENDBOT_REDIS_PORT", 6379)
+
 # Add prometheus wsgi middleware to route /metrics requests and create Gauges
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {"/metrics": make_wsgi_app()})
 export_size_gauge = Gauge("friendbot_export_size", "Size of export file in bytes")
@@ -17,30 +23,28 @@ user_gauge = Gauge("friendbot_slack_users", "Number of Users Loaded from Export"
 channel_gauge = Gauge("friendbot_slack_channels", "Number of Channels Loaded from Export")
 model_gauge = Gauge("friendbot_text_models", "Number of Text Models Generated")
 
+# Set log level
 if __name__ != "__main__":
     gunicorn_logger = logging.getLogger("gunicorn.error")
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
 
 # Check for signing secret
-signing_secret = None
-if signing_secret_file := os.environ.get("FRIENDBOT_SECRET_FILE"):
+if signing_secret_file:
     with open(signing_secret_file, "r") as f:
         signing_secret = f.readline().replace("\n", "")
     app.logger.info("Signing secret loaded")
 else:
+    signing_secret = None
     app.logger.warning("Signing secret not set! Requests will not be verified")
 
-# Check export size
-export_zip = os.environ.get("FRIENDBOT_EXPORT_ZIP", "/home/friendbot/export.zip")
-export_size = os.stat(export_zip).st_size
-export_size_gauge.set(export_size)
-
-# Load export
+# Load export and check size
 app.logger.info(f"Loading export data from {export_zip}")
 load_start_time = time.time()
 users, channels, message_data = utils.read_export(export_zip)
 load_time = round(time.time() - load_start_time, 3)
+export_size = os.stat(export_zip).st_size
+export_size_gauge.set(export_size)
 app.logger.info(f"Loaded {export_size} bytes of data in {load_time}s")
 
 # Print/Set Export metrics
@@ -62,10 +66,7 @@ app.logger.info(f"{model_count} text models generated in {model_time}s")
 model_gauge.set(model_count)
 
 # Check if Redis is available and warm up cache
-redis_host = os.environ.get("FRIENDBOT_REDIS_HOST", "redis")
-redis_port = os.environ.get("FRIENDBOT_REDIS_PORT", 6379)
 cache = redis.Redis(host=redis_host, port=redis_port)
-
 try:
     cache.ping()
     app.logger.info("Warming up Redis cache...")
